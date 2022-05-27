979e8f0817e04345b051dc14d643eff69b053a11
chloe-dev <chloe.devops@gmail.com>
2022-05-27 17:14:50 -0500

    Add Modules and API Variables

---
+++ main.py
@@ -0,0 +1,12 @@
+import secrets
+import tweepy
+import time
+import pandas
+
+api_key = secrets.api_key
+api_key_secret = secrets.api_key_secret
+acces_token = secrets.access_token
+access_token_secret = secrets.access_token_secret
+
+
+
