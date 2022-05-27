9b07a9f7f41a9b759126964d117c3a7730766e57
chloe-dev <chloe.devops@gmail.com>
2022-05-27 17:40:33 -0500

    Add Auth and API

--- main.py
+++ main.py
@@ -8,5 +8,9 @@ api_key_secret = secrets.api_key_secret
 acces_token = secrets.access_token
 access_token_secret = secrets.access_token_secret
 
+auth = tweepy.OAuth1UserHandler(
+   api_key, api_key_secret, access_token, access_token_secret
+)
 
+api = tweepy.API(auth)
 
